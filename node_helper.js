816402ab2a99232cc219e878ff9f6d66b29f5fe8
Max Isom <hi@maxisom.me>
2020-09-21 19:55:11 -0400

    Limit concurrent logins

--- node_helper.js
+++ node_helper.js
@@ -1,34 +1,37 @@
 const NodeHelper = require('node_helper');
 const AnyList = require('anylist');
-
-let calls = 0;
+const queue = require('async/queue');
 
 module.exports = NodeHelper.create({
 	start() {
 		console.log(`Starting node helper: ${this.name}`);
+
+		// Limit concurrency, otherwise we get errors when
+		// loging in.
+		this.queue = queue(async (task, callback) => {
+			const {notification, payload} = task;
+
+			if (notification === 'INIT') {
+				const {email, password, list} = payload;
+
+				this.anylist = new AnyList({email, password});
+
+				// Send update when list is updated
+				this.anylist.on('lists-update', () => {
+					this.sendSocketNotification('LIST_DATA', this.anylist.getListByName(list));
+				});
+
+				await this.anylist.login();
+				await this.anylist.getLists();
+
+				this.sendSocketNotification('LIST_DATA', this.anylist.getListByName(list));
+			}
+
+			callback();
+		}, 1);
 	},
 
 	async socketNotificationReceived(notification, payload) {
-		calls++;
-
-		if (calls % 2 === 0) {
-			await new Promise(resolve => setTimeout(resolve, 3000));
-		}
-
-		if (notification === 'INIT') {
-			const {email, password, list} = payload;
-
-			this.anylist = new AnyList({email, password});
-
-			// Send update when list is updated
-			this.anylist.on('lists-update', () => {
-				this.sendSocketNotification('LIST_DATA', this.anylist.getListByName(list));
-			});
-
-			await this.anylist.login();
-			await this.anylist.getLists();
-
-			this.sendSocketNotification('LIST_DATA', this.anylist.getListByName(list));
-		}
+		this.queue.push({notification, payload});
 	}
 });
